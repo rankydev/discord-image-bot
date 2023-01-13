e126c0102af39443cb9be6c7d5e6087faab0eb61
Ranko Spegar <ranko.spegar@diva-e.com>
2023-01-13 10:06:25 +0100

    add env variables

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	token: process.env.BOT_TOKEN,
+	clientId: process.env.BOT_CLIENT_ID,
+	guildId: process.env.BOT_GUILD_ID,
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const { token } = require("./config.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
